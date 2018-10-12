8b9a4db4fd6bf50831f8f228644bd2a3af0eca2f
iwate <iwatesoftware@live.jp>
2018-10-12 13:35:24 +0900

    fix serviceworker update

--- public/src/sw-register.js
+++ public/src/sw-register.js
@@ -1,9 +1,9 @@
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/service-worker.js')
-                       .catch(console.error.bind(console));
-    navigator.serviceWorker.getRegistration().then(registration => {
-        registration.update();
-    })
+                           .then(registration => {
+                               registration.update();
+                           })
+                           .catch(console.error.bind(console));
 }
 
 
